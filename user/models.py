342598df32efbe552d4fdd8440d4d393d4aa8167
Dmitriy Lyapin <hellishlemonade111@gmail.com>
2025-08-09 17:53:44 +0300

    Change user model (delete username field)

--- user/models.py
+++ user/models.py
@@ -7,13 +7,16 @@ from hotel.constants import NAME_MAX_LENGTH
 class Profile(AbstractUser):
 
     email = models.EmailField('Email', unique=True)
+    USERNAME_FIELD = 'email'
+    username = None
     first_name = models.CharField('Имя', max_length=NAME_MAX_LENGTH)
     last_name = models.CharField('Фамилия', max_length=NAME_MAX_LENGTH)
+    REQUIRED_FIELDS = [first_name, last_name]
 
     class Meta:
         verbose_name = 'пользователь'
         verbose_name_plural = 'Пользователи'
-        ordering = ('username',)
+        ordering = ('email',)
 
     def __str__(self):
         return self.username
