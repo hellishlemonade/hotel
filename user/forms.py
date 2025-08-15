b0c5c22bf7ebbd6d545159b0e8765d4a80610cd6
Dmitriy Lyapin <hellishlemonade111@gmail.com>
2025-08-15 20:52:31 +0300

    Frontend registration 1.0 version and add user form settings

--- user/forms.py
+++ user/forms.py
@@ -1,3 +1,6 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, HTML, ButtonHolder, Submit, Field
+from crispy_forms.bootstrap import AppendedText
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
@@ -22,4 +25,20 @@ class SignUpForm(UserCreationForm):
             'last_name',
             'password1',
             'password2'
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            HTML('<div class="my-form">Регистрация</div>'),
+            Field('email'),
+            Field('first_name'),
+            Field('last_name'),
+            Field('password1'),
+            Field('password2'),
+            ButtonHolder(
+                Submit(
+                    'submit', 'Зарегистрироваться', css_class='button white')
+            )
         )
